a1b808fd7b8f9868f83c15cbdcf7f39e79d12957
harvasyuk <harvasyuk@gmail.com>
2017-03-25 22:27:11 +0200

    Added a few buttons

--- Library/Form1.Designer.cs
+++ Library/Form1.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startingText = new System.Windows.Forms.Label();
+            this.readerButton = new System.Windows.Forms.Button();
+            this.librarianButton = new System.Windows.Forms.Button();
+            this.headButton = new System.Windows.Forms.Button();
+            this.decButton = new System.Windows.Forms.Button();
+            this.whoiamPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.whoiamPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // startingText
+            // 
+            this.startingText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.startingText.Font = new System.Drawing.Font("Calibri Light", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startingText.Location = new System.Drawing.Point(0, 0);
+            this.startingText.Name = "startingText";
+            this.startingText.Size = new System.Drawing.Size(834, 441);
+            this.startingText.TabIndex = 1;
+            this.startingText.Text = "label1";
+            this.startingText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // readerButton
+            // 
+            this.readerButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.readerButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.readerButton.FlatAppearance.BorderSize = 0;
+            this.readerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.readerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.readerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.readerButton.Location = new System.Drawing.Point(209, 3);
+            this.readerButton.Name = "readerButton";
+            this.readerButton.Size = new System.Drawing.Size(200, 50);
+            this.readerButton.TabIndex = 2;
+            this.readerButton.Text = "Я читач";
+            this.readerButton.UseVisualStyleBackColor = false;
+            this.readerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // librarianButton
+            // 
+            this.librarianButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.librarianButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.librarianButton.FlatAppearance.BorderSize = 0;
+            this.librarianButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.librarianButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.librarianButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.librarianButton.Location = new System.Drawing.Point(3, 59);
+            this.librarianButton.Name = "librarianButton";
+            this.librarianButton.Size = new System.Drawing.Size(200, 50);
+            this.librarianButton.TabIndex = 3;
+            this.librarianButton.Text = "Я бібліотекар";
+            this.librarianButton.UseVisualStyleBackColor = false;
+            // 
+            // headButton
+            // 
+            this.headButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.headButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.headButton.FlatAppearance.BorderSize = 0;
+            this.headButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.headButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.headButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.headButton.Location = new System.Drawing.Point(3, 3);
+            this.headButton.Name = "headButton";
+            this.headButton.Size = new System.Drawing.Size(200, 50);
+            this.headButton.TabIndex = 4;
+            this.headButton.Text = "Я завідувач";
+            this.headButton.UseVisualStyleBackColor = false;
+            // 
+            // decButton
+            // 
+            this.decButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
+            this.decButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.decButton.FlatAppearance.BorderSize = 0;
+            this.decButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.decButton.Location = new System.Drawing.Point(209, 59);
+            this.decButton.Name = "decButton";
+            this.decButton.Size = new System.Drawing.Size(200, 50);
+            this.decButton.TabIndex = 5;
+            this.decButton.Text = "Я декадент";
+            this.decButton.UseVisualStyleBackColor = false;
+            this.decButton.Click += new System.EventHandler(this.decButton_Click);
+            // 
+            // whoiamPanel
+            // 
+            this.whoiamPanel.Controls.Add(this.headButton);
+            this.whoiamPanel.Controls.Add(this.readerButton);
+            this.whoiamPanel.Controls.Add(this.librarianButton);
+            this.whoiamPanel.Controls.Add(this.decButton);
+            this.whoiamPanel.Location = new System.Drawing.Point(219, 202);
+            this.whoiamPanel.Name = "whoiamPanel";
+            this.whoiamPanel.Size = new System.Drawing.Size(412, 123);
+            this.whoiamPanel.TabIndex = 6;
+            this.whoiamPanel.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 286);
+            this.AutoSize = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(834, 441);
+            this.Controls.Add(this.whoiamPanel);
+            this.Controls.Add(this.startingText);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.whoiamPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label startingText;
+        private System.Windows.Forms.Button readerButton;
+        private System.Windows.Forms.Button librarianButton;
+        private System.Windows.Forms.Button headButton;
+        private System.Windows.Forms.Button decButton;
+        private System.Windows.Forms.FlowLayoutPanel whoiamPanel;
     }
 }
 
--- Library/Form1.cs
+++ Library/Form1.cs
@@ -12,9 +12,63 @@ namespace Library
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            Label startingText = new Label();
+            timer1.Start();
+
+            Label WhoIAm = new Label();
+            WhoIAm.AutoSize = true;
+            WhoIAm.Width = 140;
+            WhoIAm.Text = "Хто я?";
+            WhoIAm.ForeColor = System.Drawing.ColorTranslator.FromHtml("#ffffff");
+            WhoIAm.Font = new System.Drawing.Font("Calibri Light", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            WhoIAm.Left = (this.ClientSize.Width - WhoIAm.Width) / 2;
+            WhoIAm.Top = (this.ClientSize.Height - WhoIAm.Height) / 2 - 90;
+            this.Controls.Add(WhoIAm);
+        }
+
+        int timeLeft = 150;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startingText.ForeColor = System.Drawing.Color.White;
+
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                if (timeLeft > 120)
+                {
+                    startingText.Text = "Вітання";
+                }
+                else if (timeLeft > 50)
+                {
+                    startingText.Text = "Спершу потрібно налаштувати програму";
+                }
+                else
+                {
+                    startingText.Text = "Почнемо";
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                ActiveForm.Controls.Remove(startingText);
+                startingText = null;
+                whoiamPanel.Visible = true;
+            }    
+   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void decButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wow");
         }
     }
 }
